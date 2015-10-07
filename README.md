ea6a94f125d94f7f99b7e7cb627d33e365e7de15
Da-Me <damekim1206@gmail.com>
2015-10-06 22:22:40 -0400

    update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+
+
+## Database Structure 
+![alt tag](https://github.com/Ru-T/gradebook/blob/master/app/assets/images/database_structure.png)
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,9 +9,9 @@ class DashboardController < ApplicationController
       t = Teacher.find_by_email(params[:email])
         if t.authenticate(params[:password])
           session[:logged_in] = true
-          redirect_to new_teacher_path, notice: "You did it!"
+          redirect_to new_teacher_path, notice: "Login Success"
         else
-          redirect_to dashboard_new_path, notice: "Go jump off a short ledge."
+          redirect_to dashboard_new_path, notice: "Login Denied"
         end
       end
     end
