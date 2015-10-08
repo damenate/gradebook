1788be7d55c781cee7fee4fb00b55de63a5dabbf
Da-Me <damekim1206@gmail.com>
2015-10-07 21:39:12 -0400

    add student and parent authentication to controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,12 +5,40 @@ class DashboardController < ApplicationController
   end
 
   def create
+        if request.post?
+          teacher = Teacher.find_by_email(params[:email])
+          parent = Parent.find_by_email(params[:email])
+          student = Student.find_by_email(params[:email])
+          if teacher && teacher.authenticate(params[:password])
+            session[:logged_in_teacher] = true
+            redirect_to teachers_path, notice: "You successfully logged in."
+          elsif student && student.authenticate(params[:password])
+            session[:logged_in_student] = true
+            redirect_to student_path(student.id), notice: "You successfully logged in."
+          elsif parent && parent.authenticate(params[:password])
+            session[:logged_in_parent] = true
+            redirect_to student_path(parent.student_id), notice: "You successfully logged in."
+          else
+            redirect_to login_path, notice: "Your login was not successful."
+          end
+        end
+      end
+
+
+
+
+
     if request.post?
       t = Teacher.find_by_email(params[:email])
+      rent = Parent.find_by_email(params[:email])
+      s = Student.find_by_email(params[:email])
         if t && t.authenticate(params[:password])
           session[:logged_in_teacher] = true
           redirect_to teachers_path, notice: "Login Success"
-        else
+        elsif s && s.authenticate(params[:password])
+          session[:logged_in_student] = true
+          redirect_to student_path(s.id), notice: "Login Success."
+
           redirect_to login_path, notice: "Login Denied"
         end
       end
