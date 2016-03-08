36bf8ef205eda7649afb80e14ad8453ab93599dc
Da-Me <damekim1206@gmail.com>
2016-03-08 12:28:06 -0500

    add thoughtbot administrate

---
+++ app/dashboards/teacher_dashboard.rb
@@ -0,0 +1,60 @@
+require "administrate/base_dashboard"
+
+class TeacherDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    students: Field::HasMany,
+    id: Field::Number,
+    name: Field::String,
+    email: Field::String,
+    password_digest: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :students,
+    :id,
+    :name,
+    :email,
+  ]
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :students,
+    :id,
+    :name,
+    :email,
+    :password_digest,
+    :created_at,
+    :updated_at,
+  ]
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :students,
+    :name,
+    :email,
+    :password_digest,
+  ]
+
+  # Overwrite this method to customize how teachers are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(teacher)
+  #   "Teacher ##{teacher.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :grades
+resources :parents
+resources :students
+resources :teachers
+
+    root to: "grades#index"
+  end
+
   resources :parents
   resources :grades
   resources :students
